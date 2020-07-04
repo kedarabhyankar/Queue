6c56483f1bd2b0bb8404f8821dc10c7a3f380878
Kedar Abhyankar <krabhyankar@gmail.com>
2020-07-03 21:19:00 -0700

    Modified SceneDelegate.swift to include auto-login screen flow, so that when logged in, user state stays as logged in and isn't prompted for login on each session.

--- Queue/SceneDelegate.swift
+++ Queue/SceneDelegate.swift
@@ -17,7 +17,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        let loggedIn = UserDefaults.standard.bool(forKey: "loggedIn")
+        if(loggedIn){
+            self.window?.rootViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier: "homeTabBar")
+        } else {
+            self.window?.rootViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier: "login")
+        }
+        self.window?.makeKeyAndVisible()
         guard let _ = (scene as? UIWindowScene) else { return }
+        
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
